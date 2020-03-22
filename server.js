ca92fab7a6ba63a13883be15bdfa87b59b4bfe64
Matheus Gomes <m.gomescontato@gmail.com>
2020-03-22 19:49:30 -0300

    update port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/users', require('./users/users.controller'));
 app.use(errorHandler);
 
 // start server
-const port = 3000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
-    console.log('Server on port ' + port);
+    console.log('Server listening on port ' + port);
 });
